87875bc8d75af850ed0b02ec614ac2c07bd9d72c
Cr-Israel <carlosisrael08@hotmail.com>
2023-07-25 11:12:24 -0300

    feat: added axios and implement test

--- src/controller/Pet/Create.spec.ts
+++ src/controller/Pet/Create.spec.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from "vitest";
 import Pet from "../../models/Pets";
+import fastify from "fastify";
+import axios from "axios";
+import { Create } from "./Create";
 
 describe("Create a Pet", () => {
   it("Should be able to create a pet", () => {
@@ -12,13 +15,70 @@ describe("Create a Pet", () => {
     expect(pet).toBeInstanceOf(Pet);
   });
 
-  it("should not be able to create a pet", () => {
-    const pet = new Pet({
-      name: "Thor",
-      age: "2 anos",
-      description: "Um lindo Pastor Alemão!",
-    });
+  const server = fastify();
 
-    expect(pet).toBeInstanceOf(Error);
+  server.post("/create-pet", async (req, reply) => {
+    try {
+      await Create(req, reply);
+    } catch (error) {
+      reply.code(500).send({ message: "Erro interno no servidor" });
+    }
+  });
+  it("should not be able to create a pet with missing name", async () => {
+    try {
+      const response = await axios.post("/create-pet", {
+        name: "",
+        age: "2 anos",
+        description: "Um lindo Pastor Alemão!",
+      });
+      // Verifique se a resposta tem status 400 (Bad Request)
+      expect(response.status).toBe(400);
+
+      // Verifique a mensagem de erro na resposta
+      expect(response.data.message).toBe(
+        "O nome é obrigatório, tente novamente!"
+      );
+    } catch (error) {
+      // Se ocorrer algum erro na requisição, exiba-o no console
+      console.error("Erro na requisição: " + error);
+    }
+  });
+  it("should not be able to create a pet with missing age", async () => {
+    try {
+      const response = await axios.post("/create-pet", {
+        name: "Thor",
+        age: "",
+        description: "Um lindo Pastor Alemão!",
+      });
+      // Verifique se a resposta tem status 400 (Bad Request)
+      expect(response.status).toBe(400);
+
+      // Verifique a mensagem de erro na resposta
+      expect(response.data.message).toBe(
+        "O nome é obrigatório, tente novamente!"
+      );
+    } catch (error) {
+      // Se ocorrer algum erro na requisição, exiba-o no console
+      console.error("Erro na requisição: " + error);
+    }
+  });
+  it("should not be able to create a pet with missing description", async () => {
+    try {
+      const response = await axios.post("/create-pet", {
+        name: "Thor",
+        age: "2 anos",
+        description: "",
+      });
+      // Verifique se a resposta tem status 400 (Bad Request)
+      expect(response.status).toBe(400);
+
+      // Verifique a mensagem de erro na resposta
+      expect(response.data.message).toBe(
+        "O nome é obrigatório, tente novamente!"
+      );
+    } catch (error) {
+      // Se ocorrer algum erro na requisição, exiba-o no console
+      console.error("Erro na requisição: " + error);
+    }
   });
 });
